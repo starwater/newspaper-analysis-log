ca2ad135a9c69c943ecc23bcd5bbe492c7a5ccb0
vagrant <vagrant@vagrant.vm>
2019-04-14 18:12:24 +0000

    initial commit

---
+++ news.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# A buggy web service in need of a database.
+
+from flask import Flask, request, redirect, url_for
+
+from newsdb import get_posts, add_post, top_three, popular_author, bug_percentile
+
+app = Flask(__name__)
+
+# HTML template for the forum page
+HTML_WRAP = '''\
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DB statistics</title>
+    <style>
+      h1, form { text-align: center; }
+      textarea { width: 400px; height: 100px; }
+      div.post { text-align:center;}
+      div.t{text-align:center;}
+    </style>
+  </head>
+  <body>
+    <h1>Log statistics</h1>
+    <!-- post content will go here -->
+    <div class=t><strong>Top 3 articles</strong></div>
+%s
+<br>
+    <div class=t><strong>Popular authors of all time</strong></div>
+%s
+<br>
+    <div class=t><strong>Bugs</strong></div>
+%s
+<br>
+  </body>
+</html>
+'''
+
+# HTML template for an individual comment
+TOP3 = '''\
+    <div class=post>%s---%s votes</div>
+'''
+
+MOST_POP = '''\
+    <div class=post>%s---%s views</div>
+'''
+
+BUGS = '''\
+    <div class=post>%s---%s percentile errors</div>
+'''
+
+
+@app.route('/', methods=['GET'])
+
+def main():
+  '''Main page of the forum.'''
+  top3 = "".join(TOP3 % (title, num) for title, num in top_three())
+  pop =  "".join(MOST_POP % (name, total) for name, total in popular_author())
+  bugs =  "".join(BUGS % (date_by_day, bug_p) for date_by_day, bug_p in bug_percentile())
+  html = HTML_WRAP % (top3,pop,bugs)
+  return html
+
+@app.route('/', methods=['POST'])
+def post():
+  '''New post submission.'''
+  message = request.form['content']
+  add_post(message)
+  return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8000)
---
+++ readme.md
@@ -0,0 +1,3 @@
+The program generate web template statistics using localhost:8000
+Run python news.py to start up the server
+The sql queries are stored in newsdb.py
